4273b6327dd2d9e262984c2d0c5fcb6ebb0f54ae
Tom Baker <thomasmebaker@gmail.com>
2023-12-13 11:45:57 +0000

    inital commity

---
+++ Source/MediaPlayerTest/MediaPlayerTest.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MediaPlayerTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MediaPlayerTest, "MediaPlayerTest" );
+ 
---
+++ Source/MediaPlayerTest/MediaPlayerTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MediaPlayerTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMediaPlayerTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMediaPlayerTestGameMode();
+};
+
+
+
